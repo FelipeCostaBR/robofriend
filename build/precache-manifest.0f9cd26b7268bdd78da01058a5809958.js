a613187197fe91c1e06fce35b3eb66e8938811e6
Felipe Costae <felipejsilvacosta@gmail.com>
2020-08-20 19:05:17 +1000

    First react app

---
+++ build/precache-manifest.0f9cd26b7268bdd78da01058a5809958.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c43ef0aed3b1f09bd238a5193cc064",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "9f49cf13d414a5cbcb86",
+    "url": "/robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "617c09d34838f3f19bb9",
+    "url": "/robofriend/static/css/main.997fd0d6.chunk.css"
+  },
+  {
+    "revision": "9f49cf13d414a5cbcb86",
+    "url": "/robofriend/static/js/2.53864e7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.53864e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617c09d34838f3f19bb9",
+    "url": "/robofriend/static/js/main.2f19ceea.chunk.js"
+  },
+  {
+    "revision": "b9549992ed9aa0bec102",
+    "url": "/robofriend/static/js/runtime-main.b099a5de.js"
+  }
+]);
